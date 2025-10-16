6fd27b5f72c6b402d85977c7b8cb4fd64527d52a
carloandreaguilar <carloandreaguilar@gmail.com>
2025-10-16 15:18:22 +0200

    Initial Commit

---
+++ Cats App/Cats_AppApp.swift
@@ -0,0 +1,32 @@
+//
+//  Cats_AppApp.swift
+//  Cats App
+//
+//  Created by Carlo André Aguilar on 16/10/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Cats_AppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
